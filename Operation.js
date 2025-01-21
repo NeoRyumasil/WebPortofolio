1678e4d7144f93303ae0eacb37b3bfc0017325b3
Muhammad Alvin Ababil <96902735+NeoRyumasil@users.noreply.github.com>
2025-01-21 07:32:11 +0700

    Add files via upload

---
+++ Operation.js
@@ -0,0 +1,6 @@
+function printCV(CV){
+    const cvUrl = 'Datas/CV_MUHAMMAD ALVIN ABABIL.pdf';
+    window.location.href = cvUrl;
+}
+
+document.getElementById('printCVButton').addEventListener('click', printCV);

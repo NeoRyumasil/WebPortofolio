ba37ef37b767efaf204a8cbeef136a3499d103e6
Alvin Ababil <neoryumasil@gmail.com>
2026-01-15 21:31:50 +0800

    Remake The Portofolio using NextJS

--- Operation.js
+++
@@ -1,6 +0,0 @@
-function printCV(CV){
-    const cvUrl = 'Datas/CV_MUHAMMAD ALVIN ABABIL.pdf';
-    window.location.href = cvUrl;
-}
-
-document.getElementById('printCVButton').addEventListener('click', printCV);

6c775c7807bee4f3831a751ae608b0b8f6d4d2da
Alvin Ababil <neoryumasil@gmail.com>
2026-02-16 00:00:08 +0800

    UPDATE : Justify Description Text

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -46,7 +46,7 @@ export default function ProjectCard({ title, imageSrc, desc, link, stack, catego
                 </h3>
                 
                 {/* Description */}
-                <p className="text-sm text-gray-600 leading-relaxed mb-6 line-clamp-7">
+                <p className="text-sm text-gray-600 leading-relaxed mb-6 line-clamp-7 text-justify">
                     {desc}
                 </p>    
 
